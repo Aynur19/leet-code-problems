06c88312293a1ca70ef6b31b220dbe86fc1615b1
Aynur19 <nasybullin.aynur@yandex.ru>
2023-08-29 21:00:38 +0300

    added: problem 1071 & unit tests;

--- swift/LeetCodeProblems/LeetCodeProblemsTests/LeetCode75Tests.swift
+++ swift/LeetCodeProblems/LeetCodeProblemsTests/LeetCode75Tests.swift
@@ -8,6 +8,26 @@
 import XCTest
 
 final class LeetCode75Tests: XCTestCase {
+    // MARK: Problem 1071: Merge Strings Alternately
+    lazy var testsData_problem_1071: [(str1: String, str2: String, result: String)] = {
+        var testsData = [(str1: String, str2: String, result: String)]()
+        
+        testsData.append((str1: "ABCABC", str2: "ABC", result: "ABC"))
+        testsData.append((str1: "ABABAB", str2: "ABAB", result: "AB"))
+        testsData.append((str1: "LEET", str2: "CODE", result: ""))
+        testsData.append((str1: "TAUXXTAUXXTAUXXTAUXXTAUXX", str2: "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX", result: "TAUXX"))
+        testsData.append((str1: "AAAAAAAAA", str2: "AAACCC", result: ""))
+        
+        return testsData
+    }()
+    
+    func tests_problem_1071_gcdOfStrings() throws {
+        for data in testsData_problem_1071 {
+            XCTAssertEqual(problem_1071_gcdOfStrings(data.str1, data.str2), data.result)
+        }
+    }
+    
+    
     // MARK: Problem 1768: Merge Strings Alternately
     lazy var testsData_problem_1768: [(word1: String, word2: String, result: String)] = {
         var testsData = [(word1: String, word2: String, result: String)]()
