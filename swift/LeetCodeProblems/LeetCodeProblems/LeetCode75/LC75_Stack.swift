3cd17ecd62dc55087439482215fa32a9e4b898b7
Aynur19 <nasybullin.aynur@yandex.ru>
2023-09-07 13:06:04 +0300

    added: solution of problem 735 & unit tests;

--- swift/LeetCodeProblems/LeetCodeProblems/LeetCode75/LC75_Stack.swift
+++ swift/LeetCodeProblems/LeetCodeProblems/LeetCode75/LC75_Stack.swift
@@ -21,4 +21,32 @@ public final class LC75_Stack {
         
         return String(stack)
     }
+    
+    static func problem_735_asteroidCollision(_ asteroids: [Int]) -> [Int] {
+        var stack = [Int]()
+        stack.append(asteroids[0])
+        
+        var i = 1
+        while i < asteroids.count {
+            let tmp = asteroids[i]
+            if let last = stack.last {
+                if tmp > 0 || last < 0 && tmp < 0 {
+                    stack.append(asteroids[i])
+                } else {
+                    let sum = tmp + last
+                    if sum <= 0 {
+                        _ = stack.removeLast()
+                    }
+                    
+                    if sum < 0 { i -= 1 }
+                }
+            } else {
+                stack.append(asteroids[i])
+            }
+            
+            i += 1
+        }
+        
+        return stack
+    }
 }
--- swift/LeetCodeProblems/LeetCodeProblemsTests/LC75_Stack_Tests.swift
+++ swift/LeetCodeProblems/LeetCodeProblemsTests/LC75_Stack_Tests.swift
@@ -26,4 +26,24 @@ final class LC75_Stack_Tests: XCTestCase {
             XCTAssertEqual(actual, data.expected, "s: \(data.s)")
         }
     }
+    
+    // MARK: Problem 735. Asteroid Collision
+    private lazy var testsData_problem_735: [(asteroids: [Int], expected: [Int])] = {
+        var testsData = [(asteroids: [Int], expected: [Int])]()
+        
+        testsData.append((asteroids: [5, 10, -5], expected: [5, 10]))
+        testsData.append((asteroids: [8, -8], expected: []))
+        testsData.append((asteroids: [10, 2, -5], expected: [10]))
+        testsData.append((asteroids: [-2, -1, 1, 2], expected: [-2, -1, 1, 2]))
+        
+        return testsData
+    }()
+    
+    func tests_problem_735_asteroidCollision() throws {
+        for data in testsData_problem_735 {
+            let actual = LC75_Stack.problem_735_asteroidCollision(data.asteroids)
+            
+            XCTAssertEqual(actual, data.expected, "asteroids: \(data.asteroids)")
+        }
+    }
 }
