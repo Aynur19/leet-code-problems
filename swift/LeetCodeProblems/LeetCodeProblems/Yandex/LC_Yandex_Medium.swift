db4c85d1f0bfe1a5682ce71a314ea25597411d32
Aynur19 <nasybullin.aynur@yandex.ru>
2023-09-12 12:33:21 +0300

    added: solution of problem 228 & unit tests;

--- swift/LeetCodeProblems/LeetCodeProblems/Yandex/LC_Yandex_Medium.swift
+++ swift/LeetCodeProblems/LeetCodeProblems/Yandex/LC_Yandex_Medium.swift
@@ -83,4 +83,36 @@ final class LC_Yandex_Medium {
         return maxLength
     }
     
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    static func problem_228_summaryRanges(_ nums: [Int]) -> [String] {
+        guard !nums.isEmpty else { return [] }
+        guard nums.count > 1 else { return ["\(nums[0])"] }
+
+        var result = [String]()
+        var left = 0
+        var right = left + 1
+
+        while right < nums.count {
+            if nums[right] - nums[right - 1] != 1 {
+                if left == right - 1 {
+                    result.append("\(nums[left])")
+                } else {
+                    result.append("\(nums[left])->\(nums[right - 1])")
+                }
+
+                left = right
+            }
+
+            right += 1
+        }
+
+        if left == right - 1 {
+            result.append("\(nums[left])")
+        } else {
+            result.append("\(nums[left])->\(nums[right - 1])")
+        }
+
+        return result
+    }
 }
--- swift/LeetCodeProblems/LeetCodeProblemsTests/LC_Yandex_Tests/LC_Yandex_Tests.swift
+++ swift/LeetCodeProblems/LeetCodeProblemsTests/LC_Yandex_Tests/LC_Yandex_Tests.swift
@@ -47,4 +47,25 @@ final class LC_Yandex_Medium_Tests: XCTestCase {
             XCTAssertEqual(actual, data.expected)
         }
     }
+    
+    
+    // MARK: Problem 228. Summary Ranges
+    lazy var testsData_problem_228: [(nums: [Int], expected: [String])] = {
+        var testsData = [(nums: [Int], expected: [String])]()
+        
+        testsData.append((nums: [0, 1, 2, 4, 5, 7],
+                          expected: ["0->2", "4->5", "7"]))
+        testsData.append((nums: [0, 2, 3, 4, 6, 8, 9],
+                          expected: ["0", "2->4", "6", "8->9"]))
+        
+        return testsData
+    }()
+    
+    func tests_problem_228_summaryRanges() throws {
+        for data in testsData_problem_228 {
+            let actual = LC_Yandex_Medium.problem_228_summaryRanges(data.nums)
+            
+            XCTAssertEqual(actual, data.expected)
+        }
+    }
 }
