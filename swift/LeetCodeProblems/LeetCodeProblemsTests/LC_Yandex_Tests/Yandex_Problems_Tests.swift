9ed4502c0cfb296f890c6fa8495c2754b3d60edb
Aynur19 <nasybullin.aynur@yandex.ru>
2023-09-22 12:28:49 +0300

    added: solution of yandex problem 9 -> 16 & unit tests;

--- swift/LeetCodeProblems/LeetCodeProblemsTests/LC_Yandex_Tests/Yandex_Problems_Tests.swift
+++ swift/LeetCodeProblems/LeetCodeProblemsTests/LC_Yandex_Tests/Yandex_Problems_Tests.swift
@@ -175,7 +175,7 @@ final class Yandex_Problems_Tests: XCTestCase {
     
     // MARK: Problem 8: 15. 3Sum
     func tests_problem_15_threeSum() throws {
-        lazy var testsData_problem_13: [(nums: [Int], expected:  [[Int]])] = {
+        lazy var testsData_problem_15: [(nums: [Int], expected:  [[Int]])] = {
             var testsData = [(nums: [Int], expected:  [[Int]])]()
             
             testsData.append((nums: [-1, 0, 1, 2, -1, -4], expected: [[-1, -1, 2], [-1, 0, 1]]))
@@ -188,13 +188,33 @@ final class Yandex_Problems_Tests: XCTestCase {
             return testsData
         }()
         
-        for data in testsData_problem_13 {
+        for data in testsData_problem_15 {
             let actual = Yandex_Problems.problem_15_threeSum(data.nums)
                 .sorted(by: { $0.sorted().lexicographicallyPrecedes($1.sorted()) })
             let expected = data.expected
                 .sorted(by: { $0.sorted().lexicographicallyPrecedes($1.sorted()) })
             
             XCTAssertEqual(actual, expected,
+                          "nums: \(data.nums)")
+        }
+    }
+    
+    
+    // MARK: Problem 9: 16. 3Sum Closest
+    func tests_problem_16_threeSumClosest() throws {
+        lazy var testsData_problem_16: [(nums: [Int], target: Int, expected:  Int)] = {
+            var testsData = [(nums: [Int], target: Int, expected:  Int)]()
+            
+            testsData.append((nums: [-1, 2, 1, -4], target: 1, expected:  2))
+            testsData.append((nums: [0, 0, 0], target: 1, expected:  0))
+            
+            return testsData
+        }()
+        
+        for data in testsData_problem_16 {
+            let actual = Yandex_Problems.problem_16_threeSumClosest(data.nums, data.target)
+            
+            XCTAssertEqual(actual, data.expected,
                           "nums: \(data.nums)")
         }
     }
