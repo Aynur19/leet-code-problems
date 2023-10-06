274f9be829ff26697ed5d804a9f9f78df5df6f0e
Aynur19 <nasybullin.aynur@yandex.ru>
2023-10-06 15:10:27 +0300

    added: list node (c++);

---
+++ cpp/data_structures/list_node.cpp
@@ -0,0 +1,7 @@
+#include "list_node.h"
+
+ListNode::ListNode() : val(0), next(nullptr) { };
+
+ListNode::ListNode(int x) : val(x), next(nullptr) { };
+
+ListNode::ListNode(int x, ListNode *next) : val(x), next(next) { };
---
+++ cpp/data_structures/list_node.h
@@ -0,0 +1,8 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    
+    ListNode();
+    ListNode(int x);
+    ListNode(int x, ListNode *next);
+};
